05bd59d2bc5cc8ceab14ddae4a02ce1e37a26b52
nitish kumar <nishkumar562@gmail.com>
2024-11-18 07:49:04 +0530

    -A Button added and its css and releative hook .

--- src/react/Component/App.jsx
+++ src/react/Component/App.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
+import "./App.css";
+import Button from "./Button/Button";
+import activation from "./Hooks/activation";
 
 const App = () => {
   return (
-    <div>
-      <h1>
-        Hello world... <br />
-        This is form react Component
-      </h1>
+    <div className="App">
+      <h1 className="title">Text Extractor</h1>
+      <Button />
     </div>
   );
 };
+
 export default App;
